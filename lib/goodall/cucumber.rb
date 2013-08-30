e32c5068f8a881a0b48f8747ac4ff71e72029e36
Matthew Nielsen <xunker@pyxidis.org>
2013-08-30 14:46:46 -0600

    Change to cucumber formatting

--- lib/goodall/cucumber.rb
+++ lib/goodall/cucumber.rb
@@ -4,9 +4,10 @@ require 'goodall/command_line_enable'
 Before do |scenario|
   if scenario.feature != $current_feature
     $current_feature = scenario.feature
-    Goodall.write("#{'-'*80}\nFeature: #{$current_feature.name}")
     Goodall.write("\n")
+    Goodall.write("#{'-'*80}\nFeature: #{$current_feature.name}")
   end
+  Goodall.write("\n")
   Goodall.write("Scenario: #{scenario.name}")
   Goodall.write("\n")
 end
